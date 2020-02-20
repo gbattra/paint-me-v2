bfbe24a6326d90ce508b476c6abc1190765fdd99
Gregory Broderick Attra <greg.attra@gmail.com>
2020-02-20 08:14:08 -0500

    created painting / style / content / gen tables

--- paint_me_server/client_service/models.py
+++ paint_me_server/client_service/models.py
@@ -6,3 +6,10 @@ class PainterRequest(models.Model):
     content_image_url = models.CharField(max_length=2048)
     recipient_email = models.CharField(max_length=100)
     date_submitted = models.DateTimeField(auto_now_add=True)
+
+
+class RequestPainting(models.Model):
+    painter_request = models.ForeignKey(PainterRequest, on_delete=models.CASCADE)
+    generated_image = models.ForeignKey('painter_service.GeneratedImage', on_delete=models.CASCADE)
+    chosen_by_user = models.BooleanField(default=False)
+    date_generated = models.DateTimeField(auto_now_add=True)
--- paint_me_server/painter_service/models.py
+++ paint_me_server/painter_service/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ContentImage(models.Model):
+    image_url = models.CharField(max_length=2048)
+
+
+class StyleImage(models.Model):
+    image_url = models.CharField(max_length=2048)
+
+
+class GeneratedImage(models.Model):
+    image_url = models.CharField(max_length=2048)
+    content_image = models.ForeignKey(ContentImage, on_delete=models.CASCADE, null=True)
+    style_image = models.ForeignKey(StyleImage, on_delete=models.CASCADE)
