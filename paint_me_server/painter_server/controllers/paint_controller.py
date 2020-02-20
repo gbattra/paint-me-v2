ad5438f79c8100deb390b1d27b4ae029bc58fee4
Gregory Broderick Attra <greg.attra@gmail.com>
2020-02-19 19:10:57 -0500

    got it to use PAINTER_CONFIGS

--- paint_me_server/painter_server/controllers/paint_controller.py
+++ paint_me_server/painter_server/controllers/paint_controller.py
@@ -1,5 +1,10 @@
-from ..lib import main as painter
+from ..lib.painter import Painter
+from ..lib.painter_configs import *
 
 
 def paint(request):
-    return painter.main()
+    for i in range(len(PAINTER_CONFIGS)):
+        painter = Painter(PAINTER_CONFIGS[i]['CONTENT_LAYERS'], PAINTER_CONFIGS[i]['STYLE_LAYERS'], PAINTER_CONFIGS[i]['CONTENT_WEIGHT'], PAINTER_CONFIGS[i]['STYLE_WEIGHT'])
+        content_image_path = 'https://i.imgur.com/nwXfz5I.jpg'
+        style_image_path = 'https://i.imgur.com/TltddGV.jpg'
+        painter.paint(content_image_path, style_image_path)
