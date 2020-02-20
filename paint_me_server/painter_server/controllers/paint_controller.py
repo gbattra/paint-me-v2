59d52fef3282dddcf1d03205935b0eba37e8dfa4
Gregory Broderick Attra <greg.attra@gmail.com>
2020-02-19 19:19:02 -0500

    moved model load out of painter / nst instatiation

--- paint_me_server/painter_server/controllers/paint_controller.py
+++ paint_me_server/painter_server/controllers/paint_controller.py
@@ -1,12 +1,16 @@
+import tensorflow as tf
+
 from ..lib.painter import Painter
 from ..lib.painter_configs import *
 
 
 def paint(request):
     content_image_path = 'https://i.imgur.com/nwXfz5I.jpg'
+    pretrained_model = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
     for i in range(len(PAINTER_CONFIGS)):
         configs = PAINTER_CONFIGS[i]
         painter = Painter(
+            pretrained_model,
             configs['CONTENT_LAYERS'],
             configs['STYLE_LAYERS'],
             configs['CONTENT_WEIGHT'],
