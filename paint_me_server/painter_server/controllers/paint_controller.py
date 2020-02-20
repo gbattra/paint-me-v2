688154674bcf3f41c2746b6e8eb184a4f127f584
Gregory Broderick Attra <greg.attra@gmail.com>
2020-02-19 19:14:20 -0500

    added style image path to configs

--- paint_me_server/painter_server/controllers/paint_controller.py
+++ paint_me_server/painter_server/controllers/paint_controller.py
@@ -3,8 +3,12 @@ from ..lib.painter_configs import *
 
 
 def paint(request):
+    content_image_path = 'https://i.imgur.com/nwXfz5I.jpg'
     for i in range(len(PAINTER_CONFIGS)):
-        painter = Painter(PAINTER_CONFIGS[i]['CONTENT_LAYERS'], PAINTER_CONFIGS[i]['STYLE_LAYERS'], PAINTER_CONFIGS[i]['CONTENT_WEIGHT'], PAINTER_CONFIGS[i]['STYLE_WEIGHT'])
-        content_image_path = 'https://i.imgur.com/nwXfz5I.jpg'
-        style_image_path = 'https://i.imgur.com/TltddGV.jpg'
-        painter.paint(content_image_path, style_image_path)
+        configs = PAINTER_CONFIGS[i]
+        painter = Painter(
+            configs['CONTENT_LAYERS'],
+            configs['STYLE_LAYERS'],
+            configs['CONTENT_WEIGHT'],
+            configs['STYLE_WEIGHT'])
+        painter.paint(content_image_path, configs['STYLE_IMAGE_PATH'])
