014de4a7e612580f7b3c9a3bf6b437cb830a623a
Gregory Broderick Attra <greg.attra@gmail.com>
2020-02-22 16:13:17 -0500

    example of working push

--- client_project/client_app/controllers/client_controller.py
+++ client_project/client_app/controllers/client_controller.py
@@ -1,22 +1,45 @@
+import json
+import time
+
+
 from django.http import HttpResponse
 from django.core import serializers
+from google.cloud import pubsub_v1
 from ..models import PainterRequest, RequestPainting
 
+PUBSUB_PROJECT_ID = 'sylvan-terra-269023'
+NEW_PAINTER_REQUEST_TOPIC = 'new-painter-request'
 
+
+# /submit-request
+# - content_image_url (string)
+# - recipient_email (string)
 def submit_request(request):
     painter_request = PainterRequest.objects.create(
         content_image_url=request.GET['content_image_url'],
         recipient_email=request.GET['recipient_email'])
 
-    return HttpResponse(painter_request.id)
+    data = {
+        'painter_request_id': painter_request.id,
+        'content_image_url': painter_request.content_image_url
+    }
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = 'projects/%s/topics/%s' % (PUBSUB_PROJECT_ID, NEW_PAINTER_REQUEST_TOPIC)
+    future = publisher.publish(topic_path, data=json.dumps(data).encode('utf-8'))
+
+    return HttpResponse(future.result())
 
 
+# /request-paintings
+# - painter_request_id (int)
 def get_paintings_for_request(request):
     paintings = RequestPainting.objects.filter(painter_request_id=request.GET['painter_request_id'])
     paintings_serialized = serializers.serialize('json', list(paintings))
     return HttpResponse(paintings_serialized)
 
 
+# /request-painting
+# - request_painting_id (int)
 def get_painting(request):
     painting = RequestPainting.objects.get(id=request.GET['request_painting_id'])
     painting_serialized = serializers.serialize('json', [painting])
--- painter_app/app.py
+++ painter_app/app.py
@@ -1,12 +1,28 @@
+from google.cloud import pubsub_v1
 from flask import Flask, Response
-from .library import painter_engine as engine
 
 app = Flask(__name__)
+
+app.config.from_envvar('APP_SETTINGS')
 
 
 @app.route('/paint')
 def paint():
-    engine.paint('https://i.imgur.com/oZ67OF4.jpg')
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = 'projects/%s/subscription/%s' % \
+        (app.config['PUBSUB_PROJECT_ID'], app.config['NEW_PAINTER_REQUEST_SUBSCRIPTION'])
+
+    def callback(message):
+        print("Received message: {}".format(message))
+        # message.ack()
+
+    streampull = subscriber.subscribe(subscription_path, callback=callback)
+
+    try:
+        streampull.result(timeout=10)
+    except:
+        streampull.cancel()
+    # engine.paint('https://i.imgur.com/oZ67OF4.jpg')
 
 
 if __name__ == '__main__':
