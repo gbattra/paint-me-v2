14e89bdd5a93813cd4054ed7d22de7d72cc1d006
Gregory Broderick Attra <greg.attra@gmail.com>
2020-02-23 18:35:58 -0500

    updated consume endpoints w /

--- client_project/client_app/urls.py
+++ client_project/client_app/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('request-paintings', client_controller.get_paintings_for_request, name='request-paintings'),
     path('request-painting', client_controller.get_painting, name='request-painting'),
 
-    path('consume-painter-request-status-updates',
+    path('consume/painter-request-status-updates',
          client_controller.consume_painter_request_status_update,
          name='consume-painter-request-status-updates'),
 
-    path('consume-new-paintings', client_controller.consume_new_paintings, name='consume-new-paintings')
+    path('consume/new-paintings', client_controller.consume_new_paintings, name='consume-new-paintings')
 ]
